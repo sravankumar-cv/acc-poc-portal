c37423d736f4772d03017271439431ec2c6adb91
imsaurabhshrijan1997@gmail.com <imsaurabhshrijan1997@gmail.com>
2020-04-03 00:33:17 +0530

    validation completed

--- src/components/register-partner/register-partner.js
+++ src/components/register-partner/register-partner.js
@@ -1,104 +1,195 @@
-import React from "react";
-import {Form,Button, Container,Card, Row, Col} from "react-bootstrap";
-import history from "../../history";
-class registerPartner extends React.Component{
-constructor(props){
+
+import React from 'react';
+import "./register-partner.css";
+//import { Container } from 'react-bootstrap';
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+const phoneNumberRegex=RegExp(
+  /^[0][1-9]\d{9}$|^[1-9]\d{9}$/
+);
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
+
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+export  default class registerPartner extends React.Component{
+  constructor(props){
     super();
-    this.state={
-        name:"",
-        phoneNo:"",
-        email:"",
-        file:null
-    }
-    this.handleOnChange = this.handleOnChange.bind(this);
-        this.handleOnSubmit = this.handleOnSubmit.bind(this);
-        this.handleOnfileSelect=this.handleOnfileSelect.bind(this);
-}
-handleOnSubmit=(e)=>{
+    this.state = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      phoneNumber:null,
+      password: null,
+      file:null,
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+        password: "",
+        file:""
+      }
+    };
+  }
+  handleSubmit = e => {
     e.preventDefault();
-    console.log('inside on submit');
-    console.log(this.state);
-    history.push({
-        pathname:'/partnerProfile',
-        state:{
-          name:this.state.name,
-          phoneNo:this.state.phoneNo,
-          email:this.state.email,
-          file:this.state.file
-        }
-    })
-    
 
-}
-handleOnChange=(event)=>{
-    this.setState({[event.target.name]:event.target.value});
-}
-handleOnfileSelect=(event)=>{
-    this.setState({file:event.target.files[0]});
-}
-render(){
+    if (formValid(this.state)) {
+      console.log(`
+        --SUBMITTING--
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        Email: ${this.state.email}
+        PhoneNumber:${this.state.phoneNumber}
+        Password: ${this.state.password}
+        file:${this.state.file}
+      `);
+      //redux action handler
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value,accept } = e.target;
+    console.log(e.target.value);
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "firstName":
+        formErrors.firstName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "lastName":
+        formErrors.lastName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      case "phoneNumber":
+          formErrors.phoneNumber = phoneNumberRegex.test(parseInt(value))
+          ? ""
+          : "Enter 10 digit mobile number";
+          break;
+      case "password":
+        formErrors.password =
+          value.length < 6 ? "minimum 6 characaters required" : "";
+        break;
+      case "pancardImg":
+        formErrors.file= accept!=='.jpg'?"only jpg file accepted":"";
+        break;
+      default:
+        break;
+    }
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  }
+  render() {
+    const { formErrors } = this.state;
     return (
-        <React.Fragment>
-        <h1>register-partner</h1>
-        <Container>
-            <Row>
-                <Col>
-        <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Register For Partner </Card.Title>
-    <Form  onSubmit={this.handleOnSubmit}>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control
-     type="email" 
-     onChange={this.handleOnChange}
-     placeholder="Enter email"
-     name="email"
-     /> 
-  </Form.Group>
-  <Form.Group controlId="formBasicName">
-    <Form.Control 
-    type="text"
-    name="name"
-    onChange={this.handleOnChange}
-    placeholder="Enter  Your Name" />
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control 
-    type="text" 
-    name="phoneNo"
-    onChange={this.handleOnChange}
-    placeholder="Enter Phone Number" />
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control
-    name="file"
-    onChange={this.handleOnfileSelect}
-    accept='.jpg'
-    type="file"/> Upload PanCard
-  </Form.Group>
-
-  {/* <Button variant="primary" type="submit">
-    Submit
-  </Button> */}
-  <Button variant="primary" style={{width:'12rem'}} type="submit">
-      {/* <Link to= {{pathname:'/partnerProfile',state:{
-          name:this.state.name,
-          phoneNo:this.state.phoneNo,
-          email:this.state.email,
-          file:this.state.file
-      }}} style={{color:"white", textDecoration:"none"}}> */}
-           Get Started 
-        {/* </Link> */}
-    </Button>
-</Form>
-    
-  </Card.Body>
-</Card>
-</Col>
-</Row>
-</Container>
-</React.Fragment>
-    )
-}
-}
-export default registerPartner
+      <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Create Account For Partner</h1>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              className={formErrors.firstName.length > 0 ? "error" : null}
+              placeholder="First Name"
+              type="text"
+              name="firstName"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.firstName.length > 0 && (
+              <span className="errorMessage">{formErrors.firstName}</span>
+            )}
+          </div>
+          <div className="lastName">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              className={formErrors.lastName.length > 0 ? "error" : null}
+              placeholder="Last Name"
+              type="text"
+              name="lastName"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.lastName.length > 0 && (
+              <span className="errorMessage">{formErrors.lastName}</span>
+            )}
+          </div>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input
+              className={formErrors.email.length > 0 ? "error" : null}
+              placeholder="Email"
+              type="email"
+              name="email"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.email.length > 0 && (
+              <span className="errorMessage">{formErrors.email}</span>
+            )}
+          </div>
+          <div className="phoneNumber">
+            <label htmlFor="phoneNumber">Mobile Number</label>
+            <input
+              className={formErrors.phoneNumber.length > 0 ? "error" : null}
+              placeholder="Mobile Number"
+              type="text"
+              name="phoneNumber"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.email.length > 0 && (
+              <span className="errorMessage">{formErrors.email}</span>
+            )}
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              className={formErrors.password.length > 0 ? "error" : null}
+              placeholder="Password"
+              type="password"
+              name="password"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.password.length > 0 && (
+              <span className="errorMessage">{formErrors.password}</span>
+            )}
+          </div>
+          <div className="myfile">
+            <input type="file"
+            name="pancardImg"
+            noValidate
+            accept='.jpg'
+            onChange={this.handleChange}
+            />
+          </div>
+          <div className="createAccount">
+            <button type="submit">Create Account</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    );
+  };
+};
--- src/components/register-user/register-user.js
+++ src/components/register-user/register-user.js
@@ -1,88 +1,180 @@
-import  React from 'react';
-import history from "../../history";
-import {Form,Button, Container,Card, Row, Col} from "react-bootstrap";
-export default  class registerUser extends React.Component{
-    constructor(props){
-        super();
-        this.state={
-            name:"",
-            phoneNo:"",
-            email:"",
-        }
-        this.handleOnChange = this.handleOnChange.bind(this);
-            this.handleOnSubmit = this.handleOnSubmit.bind(this);
-            this.handleOnfileSelect=this.handleOnfileSelect.bind(this);
-    }
-    handleOnSubmit=(e)=>{
-        e.preventDefault();
-        console.log('inside on submit');
-        console.log(this.state);
-        history.push({
-            pathname:'/userProfile',
-            state:{
-              name:this.state.name,
-              phoneNo:this.state.phoneNo,
-              email:this.state.email,
-              file:this.state.file
-            }
-        })
-        // history.push({
-        //     pathname:'/userProfile'
-        // })
-    }    
-    handleOnChange=(event)=>{
-        this.setState({[event.target.name]:event.target.value});
-    }
-    handleOnfileSelect=(event)=>{
-        this.setState({file:event.target.files[0]});
-    }
-   render(){
-       return (
-           <React.Fragment>
-               <h1>register user </h1>
-               <Container>
-            <Row>
-                <Col>
-        <Card style={{ width: '18rem' }}>
-  <Card.Body>
-    <Card.Title>Register For User </Card.Title>
-    <Form  onSubmit={this.handleOnSubmit}>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control
-     type="email" 
-     onChange={this.handleOnChange}
-     placeholder="Enter email"
-     name="email"
-     /> 
-  </Form.Group>
-  <Form.Group controlId="formBasicName">
-    <Form.Control 
-    type="text"
-    name="name"
-    onChange={this.handleOnChange}
-    placeholder="Enter  Your Name" />
-  </Form.Group>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Control 
-    type="number" 
-    name="phoneNo"
-    onChange={this.handleOnChange}
-    placeholder="Enter Phone Number" />
-  </Form.Group>
 
+import React from 'react';
+const emailRegex = RegExp(
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+);
+const phoneNumberRegex=RegExp(
+  /^[0][1-9]\d{9}$|^[1-9]\d{9}$/
+);
+const formValid = ({ formErrors, ...rest }) => {
+  let valid = true;
 
-  {/* <Button variant="primary" type="submit">
-    Submit
-  </Button> */}
-  <Button variant="primary" style={{width:'12rem'}} type="submit">Register </Button>
-</Form>
-    
-  </Card.Body>
-</Card>
-</Col>
-</Row>
-</Container>
-           </React.Fragment>
-       )
-   }
+  // validate form errors being empty
+  Object.values(formErrors).forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+
+  // validate the form was filled out
+  Object.values(rest).forEach(val => {
+    val === null && (valid = false);
+  });
+
+  return valid;
+};
+export  default class registerUser extends React.Component{
+  constructor(props){
+    super();
+    this.state = {
+      firstName: null,
+      lastName: null,
+      email: null,
+      password: null,
+      phoneNumber:null,
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        phoneNumber:"",
+        email: "",
+        password: "",
+      }
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (formValid(this.state)) {
+      console.log(`
+        --SUBMITTING--
+        First Name: ${this.state.firstName}
+        Last Name: ${this.state.lastName}
+        PhoneNumber:${this.state.phoneNumber}
+        Email: ${this.state.email}
+        Password: ${this.state.password}
+      `);
+      //redux action handler
+    } else {
+      console.error("FORM INVALID - DISPLAY ERROR MESSAGE");
+    }
+  };
+
+  handleChange = e => {
+    e.preventDefault();
+    const { name, value, accept } = e.target;
+    console.log('file event is  ',e.target);
+    let formErrors = { ...this.state.formErrors };
+
+    switch (name) {
+      case "firstName":
+        formErrors.firstName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "lastName":
+        formErrors.lastName =
+          value.length < 3 ? "minimum 3 characaters required" : "";
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? ""
+          : "invalid email address";
+        break;
+      case "password":
+        formErrors.password =
+          value.length < 6 ? "minimum 6 characaters required" : "";
+        break;
+      case "phoneNumber":
+        formErrors.phoneNumber = phoneNumberRegex.test(parseInt(value))
+        ? ""
+        : "Enter 10 digit mobile number";
+        break;
+      default:
+        break;
+    }
+    this.setState({ formErrors, [name]: value }, () => console.log(this.state));
+  }
+  render() {
+    const { formErrors } = this.state;
+    return (
+      <div className="wrapper">
+      <div className="form-wrapper">
+        <h1>Create Account For Users</h1>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input
+              className={formErrors.firstName.length > 0 ? "error" : null}
+              placeholder="First Name"
+              type="text"
+              name="firstName"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.firstName.length > 0 && (
+              <span className="errorMessage">{formErrors.firstName}</span>
+            )}
+          </div>
+          <div className="lastName">
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              className={formErrors.lastName.length > 0 ? "error" : null}
+              placeholder="Last Name"
+              type="text"
+              name="lastName"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.lastName.length > 0 && (
+              <span className="errorMessage">{formErrors.lastName}</span>
+            )}
+          </div>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input
+              className={formErrors.email.length > 0 ? "error" : null}
+              placeholder="Email"
+              type="email"
+              name="email"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.email.length > 0 && (
+              <span className="errorMessage">{formErrors.email}</span>
+            )}
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              className={formErrors.password.length > 0 ? "error" : null}
+              placeholder="Password"
+              type="password"
+              name="password"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.password.length > 0 && (
+              <span className="errorMessage">{formErrors.password}</span>
+            )}
+          </div>
+          <div className="phoneNumber">
+            <label htmlFor="phoneNumber">Mobile Number</label>
+            <input
+              className={formErrors.phoneNumber.length > 0 ? "error" : null}
+              placeholder="Mobile Number"
+              type="text"
+              name="phoneNumber"
+              noValidate
+              onChange={this.handleChange}
+            />
+            {formErrors.email.length > 0 && (
+              <span className="errorMessage">{formErrors.email}</span>
+            )}
+          </div>
+          <div className="createAccount">
+            <button type="submit">Create Account</button>
+          </div>
+        </form>
+      </div>
+    </div>
+    );
+  };
 };
