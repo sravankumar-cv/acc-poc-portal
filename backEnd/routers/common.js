acb8ee17f3c26d5d676466f2ecd9f378e4f40ac1
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2020-06-15 01:46:24 +0530

    Adding Safety regulations from expolit DB

--- backEnd/routers/common.js
+++ backEnd/routers/common.js
@@ -3,14 +3,18 @@
  */
 
 const router = require('express').Router(),
+    validationMiddleware = require('../validator/middleware'),
+    errorSchema = require('../validator/errorSchema'),
     getAllCountriesController = require('../controllers/common/getCountiesController'),
     getFinancialServiceController = require('../controllers/common/getFinanicalSerivicesController'),
     getBusinessTypeController = require('../controllers/common/getBusinessCategoriesController'),
-    searchController = require('../controllers/common/searchController');
+    searchController = require('../controllers/common/searchController'),
+    errorController = require('../controllers/common/serviceErrorController');
 
 router.get('/countries', getAllCountriesController.getCountryList);
 router.get('/services', getFinancialServiceController.getFiancialService);
 router.get('/business', getBusinessTypeController.getBusinessService);
 router.post('/search', searchController.serachByName);
+router.post('/error', validationMiddleware(errorSchema), errorController.registerError);
 
 module.exports = router;
