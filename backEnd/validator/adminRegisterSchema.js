684f5c51bf0e724a5ff079317b941bbfe4ea1dd4
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2020-07-01 17:25:32 +0530

    Fix admin validation

--- backEnd/validator/adminRegisterSchema.js
+++ backEnd/validator/adminRegisterSchema.js
@@ -1,6 +1,7 @@
 const joi = require('joi');
 
 const adminRegisterSchema = joi.object().keys({
+    name: joi.string().required().exist(),
     email: joi.string().normalize().required().exist().email(),
     password: joi.string().normalize().exist().required().regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/),
     role: joi.number().optional().valid([3])
