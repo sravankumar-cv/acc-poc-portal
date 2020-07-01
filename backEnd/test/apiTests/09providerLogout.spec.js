7ab39a6b5fd9f2bf928ae6eea4c3314ac1af25a2
Jithinqw <jithin.mscis3@iiitmk.ac.in>
2020-07-01 14:47:16 +0530

    Login provider, logput provider unit test

---
+++ backEnd/test/apiTests/09providerLogout.spec.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Unit tests for provider logout
+ * @access Protected
+ */
+
+let chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    should = require('chai').should(),
+    faker = require('faker'),
+    app = require('../../testServer');
+
+chai.use(chaiHttp);
+
+describe('Unit test for provider logou', ()=> {
+})
