ac1203a9f700cce166c0fe9874e23209f891a530
Akanksha <akankshajha45@gmail.com>
2020-07-09 17:23:36 +0530

    Provider Details API

--- backEnd/controllers/provider/getProviderDetailsController.js
+++ backEnd/controllers/provider/getProviderDetailsController.js
@@ -5,11 +5,13 @@
 
 const provideModel = require('../../model/providerModel');
 
-exports.getProviderDetails = (partnerId, res) => {
-    provideModel.find({partnerId: partnerId}, (err, providers)=> {
+exports.getProviderDetails = (req, res) => {
+    
+    provideModel.findOne({partnerId:req.params.partnerId  }, (err, providers)=> {
         if(err) {
             res.status(400).json(err);
         } else {
+            
             res.status(200).json(providers);
         }
     })
