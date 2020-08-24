109b345a4e59555c6abde1661e2eb1b1fedbd204
ravindrakumar1996 <40341496+ravindrakumar1996@users.noreply.github.com>
2020-08-24 13:23:49 +0530

    Add files via upload

--- yolo/src/components/AdminDashboard/AdminDashboard.jsx
+++ yolo/src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
+import { 
+    ExpansionPanel, ExpansionPanelDetails,
+    ExpansionPanelSummary,ExpansionPanelActions,
+    Chip, Button, Divider, Icon, Typography,
+    Snackbar
+} from '@material-ui/core';
+
 import AdminHeader from '../shared/adminHeader/adminHeader';
 import AdminApprovalContainer from '../../containers/adminApprovalContainer';
+
+import AdminDashboardContent from './AdminDashboardContent.jsx'
+
 
 export default class AdminDashboard extends React.Component {
 
@@ -11,17 +21,35 @@ export default class AdminDashboard extends React.Component {
         }
     }
 
+
     componentDidMount() {
         document.title = 'Yoloj - Admin Dashboard';
     }
 
+
+    
     render() {
         return (
             <div>
                 <AdminHeader />
-                <div style={{paddingTop:200}}>
-                    <AdminApprovalContainer unapprovedList={this.state.unapprovedProviders}/>
+
+
+                <div class="content" style={{paddingTop:100}}>
+
+                    <div class="left_content">
+                        <AdminDashboardContent/>
+                    </div>
+
+                    <div class="right_content box_shadow">
+                        <br/><Typography variant="body" style={{color: '#4d4d4d'}}>Statistical Data</Typography>
+                        <br/><br/><Divider/>
+                        <br/>    
+                    </div>
+
+                    
+                    
                 </div>
+
             </div>
         )
     }
