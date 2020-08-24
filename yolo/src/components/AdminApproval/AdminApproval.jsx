37d9e03d29f35e01b59df18328975ac46d340bc1
ravindrakumar1996 <40341496+ravindrakumar1996@users.noreply.github.com>
2020-08-24 13:21:39 +0530

    Update AdminApproval.jsx

--- yolo/src/components/AdminApproval/AdminApproval.jsx
+++ yolo/src/components/AdminApproval/AdminApproval.jsx
@@ -6,6 +6,20 @@ import {
     Snackbar
 } from '@material-ui/core';
 import { store } from '../../store';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import {Link} from 'react-router-dom';
+import DoneIcon from '@material-ui/icons/Done';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+import '../AdminDashboard/AdminDashboard.css';
+
+var approval_link_style={
+    fontSize: '14px',
+    color: '#4d4d4d',
+    fontweight: 'bold',
+    textDecoration: 'underline'
+}
 
 export default class AdminApproval extends React.Component {
 
@@ -50,27 +64,56 @@ export default class AdminApproval extends React.Component {
 
     render() {
         return(
-            <div style={{maxWidth:600, marginLeft:500}}>
+            <div>
                 {
                     (this.state.unapprovedProviders && this.state.unapprovedProviders.length) ? this.state.unapprovedProviders.map((item, index)=> {
                         return (
-                            <ExpansionPanel key={index}>
-                                <ExpansionPanelSummary
-                                    expandIcon={<Icon className="fa fa-sort-desc" aria-hidden="true"/>}>
-                                    <Typography>{item.OrganizationName}</Typography>
-                                </ExpansionPanelSummary>
-                                <ExpansionPanelDetails>
-                                    <span>Organization Address: {item.OrganizationAddress}</span>
-                                </ExpansionPanelDetails>
-                                <Divider />
-                                <ExpansionPanelActions>
-                                    <Button variant="contained" color="primary" onClick={() => this.handleAccept(item.email, true)}>Approve</Button>
-                                    <Button variant="contained" color="secondary" onClick={() => this.handleReject(item.email, false)}>Reject</Button>
-                                </ExpansionPanelActions>
-                            </ExpansionPanel>
+                            <div class="unapproved_providers_div" key={index}>
+
+                                <div class="unapproved_providers_div_left">
+                                    <img class="avatar_image" src={item.providerIdentityImg}/>
+                                </div>
+
+                                
+                                <div class="unapproved_providers_div_right">
+
+                                <Typography variant="body" style={{fontSize: '13px'}} color="text">
+                                    <strong><span style={{color: '#006699'}}>{item.fullName}</span></strong> has joined Yoloj. From {item.City}  
+                                    &nbsp;& has requested to approve for the verification.
+                                </Typography> 
+                                <br/>
+
+                                <Typography variant="caption">
+                                    Works at {item.OrganizationName}&nbsp;
+                                </Typography>
+
+                                <Typography variant="caption" style={{color: '#4d4d4d'}}>  
+                                    <AccessTimeIcon style={{fontSize:'12px'}}/>&nbsp;
+                                    {item.createdOn}
+                                </Typography>     
+                                
+
+                                <br/>
+                                
+
+                                <Link color="primary" style={approval_link_style} onClick={() => this.handleAccept(item.email, true)}>
+                                    Approve&nbsp;<DoneIcon style={{fontSize: '12px'}}/>
+                                </Link>&nbsp;
+                                <Link color="primary" style={approval_link_style} onClick={() => this.handleReject(item.email, false)}>
+                                    Reject&nbsp;<CloseIcon style={{fontSize: '13px'}}/>
+                                </Link>&nbsp;
+                                
+                                </div>    
+
+
+                            </div>
+                            
                         )
                     }) : <span>No new Providers to approve.</span>
                 }
+             
+
+
                 <Snackbar
                     anchorOrigin={{
                         vertical: 'bottom',
